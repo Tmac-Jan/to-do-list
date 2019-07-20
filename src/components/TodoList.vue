f5967d60d3ba016fcde131b6911b8ae39f0c317e
macmanboy <macmanboy@foxmail.com>
2019-07-20 11:44:25 +0800

    [feat]:[create the todolist Tab and update some data type]

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,21 +6,34 @@
     </div>
     <div class="center">
       <div class="addToDoList">
-        <input type="text" v-model="todo" placeholder="add to do item" style="width: 90%;"></input>
-        <Button >add</Button>
+        <input type="text" v-model="todoItem" placeholder="add to do item" style="width: 80%;"></input>
+        <Button @click="addToDoItem">add</Button>
       </div>
       <div class="todolist">
-        <ul class="listview" v-show="todoList.length">
+        <ul class="listView" v-show="todoList.length">
           <li v-for="item in filterList">
             <div class="todoitem">
               <input type="checkbox" v-model="item.isComplete"/>
               <label v-show="!item.editable"
-                     :class="item.isComplete ? 'taskitem' : 'text'">{{item.title}}</label>
-              <input type="text" class="editor" v-model="item.title"
+                     :class="item.isComplete ? 'taskitem' : 'text'">{{item.todoItemName}}</label>
+              <input type="text" class="editor" v-model="item.todoItemName"
                      v-show="item.editable"
                      @on-blur="" @on-enter="">
               </input>
             </div>
+          </li>
+        </ul>
+      </div>
+      <div class="todoTab">
+        <ul class="item-count">
+          <li class="action">
+            <a :class="{active:visibility='all'}" href="javascript:void(0);" @click="">ALL</a>
+          </li>
+          <li class="action">
+            <a :class="{active:visibility='unCompleted'}"  href="javascript:void(0);"  @click="">Active</a>
+          </li>
+          <li class="action">
+            <a :class="{active:visibility='completed'}" href="javascript:void(0);"  @click="">Complete</a>
           </li>
         </ul>
       </div>
@@ -33,8 +46,23 @@
     name: 'TodoList',
     data() {
       return {
-        todoList:[],
-        filterList:[]
+        todoList: [],
+        filterList: [],
+        todoItem: ''
+      }
+    },
+    methods: {
+      addToDoItem(todoItem) {
+        if (todoItem.length === 0 || todoItem === '') {
+          return;
+        } else {
+          this.todoList.push({
+            todoItemName: this.todoItem,
+            isComplete: false,
+            editable:false
+          })
+          this.todoItem='';
+        }
       }
     }
   }
@@ -49,17 +77,37 @@
   ul {
     list-style-type: none;
     padding: 0;
+    display: inline-block;
+    overflow: auto;
   }
 
   li {
     display: inline-block;
     margin: 0 10px;
   }
-.main{
-  margin: 0 auto;
-  width: 80%;
+
+  .main {
+    margin: 0 auto;
+    width: 80%;
+  }
+.todoTab{
+  text-align: center;
 }
+  .action{
+    float: left;
+    list-style: none;
+  }
   a {
+    border-radius:5px;
     color: #42b983;
+    width: 140px;
+    height: 30px;
+    text-decoration: none;
+    text-align: center;
+    line-height: 30px;
+    display: block;
+  }
+  .active{
+    border:1px solid  #42b983;
   }
 </style>
